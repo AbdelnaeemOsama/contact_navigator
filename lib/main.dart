d2e630b04b97d79426ce8b26afbf14ed929c4cd9
Waleed_Elseify <waleedraafatelseify@gmail.com>
2026-04-06 22:50:28 +0200

    true

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class ContactNavigatorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       home: const SplashScreen(),
       theme: ThemeData(
         fontFamily: AppFonts.primary,
